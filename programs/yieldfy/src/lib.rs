133908e0a4cf666781c32cda2ebe8a1a08bf7f5f
Yieldfy <yieldfy@proton.me>
2026-04-20 14:01:34 -0400

    Wire submodules: attest, deposit_ix, state, withdraw_ix
    Named deposit_ix / withdraw_ix instead of deposit / withdraw so
    the #[program] macro's generated __client_accounts_* paths don't
    collide with the instruction function names of the same word.

--- programs/yieldfy/src/lib.rs
+++ programs/yieldfy/src/lib.rs
@@ -2,3 +2,8 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("CNGH7jZbLHJDTWSz5XnbZ5o4QBWQxph6qGWKRe1y6SNK");
+
+pub mod attest;
+pub mod deposit_ix;
+pub mod state;
+pub mod withdraw_ix;
