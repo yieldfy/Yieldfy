c421139fd676d75d40d8b5d4ac19a315c5ccd1fb
Yieldfy <yieldfy@proton.me>
2026-04-20 13:59:58 -0400

    Open Withdraw Accounts struct with Config PDA
    Same [b"config"] / bump pattern as deposit. Config is not mut
    here because withdraw doesn't touch program-wide settings.

--- programs/yieldfy/src/withdraw_ix.rs
+++ programs/yieldfy/src/withdraw_ix.rs
@@ -2,3 +2,9 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Burn, Mint, Token, TokenAccount, Transfer};
 
 use crate::state::*;
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(seeds = [b"config"], bump = config.bump)]
+    pub config: Account<'info, Config>,
+}
